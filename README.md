ecaf9ec96400a08d4e5d930d63f0c768edcd4935
ividyon <1077140+ividyon@users.noreply.github.com>
2025-09-07 17:06:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+# SoulsGrassConvert
+A quick tool for Souls modding (ELDEN RING, Sekiro, Armored Core VI) to convert and edit GRASS files as GLTF files.
+
+## Credits
+
+* All SoulsFormatsNEXT contributors
+* *ivi* - Author
+
+## License
+
+This work is licensed under GPL v3. View the implications for your forks [here](https://www.tldrlegal.com/license/gnu-general-public-license-v3-gpl-3).
+
+SoulsGrassConvert is built using the following licensed works:
+* [SoulsFormats](https://github.com/JKAnderson/SoulsFormats/tree/er) by JKAnderson (see [License](licenses/LICENSE-SoulsFormats.md))
+* [PromptPlus](https://github.com/FRACerqueira/PromptPlus), Copyright 2021 @ Fernando Cerqueira (see [License](licenses/LICENSE-PromptPlus.md))
+
+## Changelog
+
+### 1.0.0.0
+
+* Initial release.
---
+++ SoulsGrassConvert/Program.cs
@@ -0,0 +1,231 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Numerics;
+using System.Text.Json;
+using PPlus;
+using SharpGLTF.Schema2;
+using SoulsFormats;
+
+namespace SoulsGrassConvert;
+
+class Program
+{
+    public static bool IsDebug()
+    {
+        return Debugger.IsAttached;
+    }
+
+    private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        var exception = e.ExceptionObject as Exception;
+        File.WriteAllText(Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory)!, "crash.log"),
+            exception?.InnerException?.ToString() ?? exception?.ToString());
+        throw exception;
+    }
+
+    private static void RunProgram(string[] args)
+    {
+        PromptPlus.DoubleDash("SoulsGrassConvert");
+
+        string? filePath = null;
+        bool automatic;
+        if (args.Any())
+        {
+            automatic = true;
+            filePath = args.First();
+            if (filePath == null || !File.Exists(filePath))
+            {
+                PromptPlus.WriteLine("File not found.");
+                return;
+            }
+        }
+        else
+        {
+            automatic = false;
+            PromptPlus.WriteLine(
+                "Hi! This is a quick tool for turning a GRASS file into a GLTF file, and vice versa.");
+            PromptPlus.WriteLine();
+            PromptPlus.KeyPress("In the next dialog, please select the file you wish to process.").Run();
+
+            while (filePath == null || !File.Exists(filePath))
+            {
+                var picker = NativeFileDialogSharp.Dialog.FileOpen();
+                if (!picker.IsOk)
+                {
+                    PromptPlus.KeyPress("Invalid file path! Try again...").Run();
+                    continue;
+                }
+
+                filePath = picker.Path;
+                break;
+            }
+        }
+
+        var filename = Path.GetFileNameWithoutExtension(filePath);
+        var ext = Path.GetExtension(filePath).ToLower();
+        if (ext == ".grass")
+        {
+            PromptPlus.WriteLine($"Reading GRASS file at {filePath}...");
+            var grass = GRASS.Read(filePath);
+
+            PromptPlus.WriteLine($"Converting GRASS to GLTF...");
+            var gltf = SharpGLTF.Schema2.ModelRoot.CreateModel();
+            var scene = gltf.DefaultScene = gltf.UseScene($"{filename}_scene");
+            var node = scene.CreateNode($"{filename}_node");
+            var mesh = node.Mesh = gltf.CreateMesh($"{filename}_mesh");
+            var material = gltf
+                .CreateMaterial($"{filename}_material")
+                .WithDoubleSide(true);
+
+            var indices = grass.Faces.Select(a => new[] { a.VertexIndexA, a.VertexIndexB, a.VertexIndexC })
+                .SelectMany(a => a).ToList().AsReadOnly();
+            var prim = mesh.CreatePrimitive()
+                .WithVertexAccessor("POSITION",
+                    grass.Vertices.Select(a => a.Position with { X = -a.Position.X }).ToArray())
+                .WithIndicesAccessor(PrimitiveType.TRIANGLES, indices)
+                .WithMaterial(material);
+            for (int i = 0; i < 6; i++)
+            {
+                prim.WithVertexAccessor($"COLOR_{i}",
+                    grass.Vertices.Select(a => new Vector3(a.GrassDensities[i], a.GrassDensities[i], a.GrassDensities[i]))
+                        .ToArray());
+            }
+
+            var gltfPath = Path.Combine(Path.GetDirectoryName(filePath)!, $"{Path.GetFileNameWithoutExtension(filePath)}.gltf");
+            if (File.Exists(gltfPath))
+            {
+                PromptPlus.WriteLine($"Backing up existing file at {gltfPath}...");
+                File.Copy(gltfPath, $"{gltfPath}.bak", true);
+            }
+            PromptPlus.WriteLine($"Writing GLTF to {gltfPath}...");
+            gltf.SaveGLTF(gltfPath);
+
+            var boundingPath = Path.Combine(Path.GetDirectoryName(filePath)!, $"{Path.GetFileNameWithoutExtension(filePath)}.json");
+            if (File.Exists(boundingPath))
+            {
+                PromptPlus.WriteLine($"Backing up existing file at {boundingPath}...");
+                File.Copy(boundingPath, $"{boundingPath}.bak", true);
+            }
+            PromptPlus.WriteLine($"Writing bounding boxes to {boundingPath}...");
+            var boundingVolumes = JsonSerializer.Serialize(grass.BoundingVolumeHierarchy);
+            File.WriteAllText(boundingPath, boundingVolumes);
+
+            if (automatic)
+                PromptPlus.WriteLine($"Wrote GLTF to {gltfPath}.");
+            else
+                PromptPlus.KeyPress($"Wrote GLTF to {gltfPath}. Press any key to exit...").Run();
+            return;
+        }
+        else if (ext == ".gltf")
+        {
+            PromptPlus.WriteLine($"Reading GLTF file at {filePath}...");
+            var loaded = SharpGLTF.Schema2.ModelRoot.Load(filePath)!;
+
+            var boundingPath = Path.Combine(Path.GetDirectoryName(filePath)!,
+                $"{Path.GetFileNameWithoutExtension(filePath)}.json");
+            if (!File.Exists(boundingPath))
+            {
+                if (automatic)
+                    PromptPlus.WriteLine($"Could not find bounding box data at {boundingPath}.");
+                else
+                    PromptPlus.KeyPress($"Could not find bounding box data at {boundingPath}. Press any key to exit...").Run();
+                return;
+            }
+            PromptPlus.WriteLine($"Reading bounding box data at {boundingPath}...");
+            var boundingJson = File.ReadAllText(boundingPath);
+            List<GRASS.Volume>? bounding = JsonSerializer.Deserialize<List<GRASS.Volume>>(boundingJson);
+            if (bounding == null)
+            {
+                if (automatic)
+                    PromptPlus.WriteLine($"Invalid bounding box data provided.");
+                else
+                    PromptPlus.KeyPress($"Invalid bounding box data provided. Press any key to exit...").Run();
+                return;
+            }
+
+            var loadedScene = loaded.DefaultScene;
+            var loadedNode = loadedScene.FindNode(n => true);
+            var loadedMesh = loadedNode.Mesh;
+            var loadedPrim = loadedMesh.Primitives.First();
+            var vertexPos = loadedPrim.GetVertexAccessor("POSITION").AsVector3Array().ToArray();
+            var faceIndices = loadedPrim.GetIndexAccessor().AsIndicesArray().ToArray();
+            var vertices = vertexPos.Select(a => new GRASS.Vertex() { Position = a with { X = -a.X } }).ToList();
+            var faces = faceIndices.Select((x, i) => new { x, i }).GroupBy(g => g.i / 3).Select(a =>
+            {
+                var face = new GRASS.Face();
+                var v = a.ToArray().Select(a => a.x).ToArray();
+                face.VertexIndexA = (int)v[0];
+                face.VertexIndexB = (int)v[1];
+                face.VertexIndexC = (int)v[2];
+                return face;
+            }).ToList();
+            var newGrass = new GRASS();
+            for (int j = 0; j < 6; j++)
+            {
+                var color = loadedPrim.GetVertexAccessor($"COLOR_{j}").AsVector3Array().ToArray();
+                for (int i = 0; i < vertices.Count; i++)
+                {
+                    var vertex = vertices[i];
+                    vertex.GrassDensities[j] = color[i].X;
+                }
+            }
+
+            newGrass.Vertices = vertices;
+            newGrass.Faces = faces;
+            newGrass.BoundingVolumeHierarchy = bounding;
+
+            var grassPath = Path.Combine(Path.GetDirectoryName(filePath)!, $"{Path.GetFileNameWithoutExtension(filePath)}.grass");
+            if (File.Exists(grassPath))
+            {
+                PromptPlus.WriteLine($"Backing up existing file at {grassPath}...");
+                File.Copy(grassPath, $"{grassPath}.bak", true);
+            }
+            PromptPlus.WriteLine($"Writing GRASS to {grassPath}...");
+            newGrass.Write(grassPath);
+
+            if (automatic)
+                PromptPlus.WriteLine($"Wrote GRASS to {grassPath}.");
+            else
+                PromptPlus.KeyPress($"Wrote GRASS to {grassPath}. Press any key to exit...").Run();
+            return;
+        }
+        else
+        {
+            if (automatic)
+                PromptPlus.WriteLine("Provided file is not a GLTF or a GRASS file.");
+            else
+                PromptPlus.KeyPress("Provided file is not a GLTF or a GRASS file. Press any key to exit...").Run();
+            return;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        PromptPlus.Config.DefaultCulture = new CultureInfo("en-us");
+        PromptPlus.IgnoreColorTokens = true;
+
+        if (!IsDebug())
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+        try
+        {
+            RunProgram(args);
+        }
+        catch (Exception e) when (!IsDebug())
+        {
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), "crash.log"),
+                e?.InnerException?.ToString() ?? e?.ToString());
+            PromptPlus.Error.WriteLine(@$"
+There was an exception:
+
+{e?.InnerException?.ToString() ?? e?.ToString()}
+
+This error message has also been saved to crash.log in the program directory.
+
+Press any key to exit...");
+            PromptPlus.ReadKey();
+        }
+    }
+}
